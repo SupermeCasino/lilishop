6c1718b7e59baa7c3ebf4c8bbb80c52c4e4ec5dc
misworga831 <paulgao0831@gmail.com>
2026-01-08 17:37:19 +0900

    fix: 替换 JSON 工具为 Jackson 并优化消息解析逻辑
    - 使用 `ObjectMapper` 替代 `JSONUtil` 解析 WebSocket 消息，增强兼容性和异常处理能力。
    - 在 `MessageOperation` 类中添加 `@JsonProperty` 和 `@JsonAlias` 注解，支持多格式字段名解析。

--- framework/src/main/java/cn/lili/modules/im/entity/vo/MessageOperation.java
+++ framework/src/main/java/cn/lili/modules/im/entity/vo/MessageOperation.java
@@ -3,6 +3,8 @@ package cn.lili.modules.im.entity.vo;
 import cn.lili.common.utils.StringUtils;
 import cn.lili.modules.im.entity.enums.MessageTypeEnum;
 import cn.lili.modules.im.entity.enums.OperationType;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -12,8 +14,10 @@ import lombok.Data;
 public class MessageOperation {
 
     /**
-     * 消息类型
+     * 操作类型
      */
+    @JsonProperty("operation_type")
+    @JsonAlias({"operationType", "operation_type"})
     private OperationType operationType;
     /**
      * 与某人聊天记录
@@ -28,11 +32,15 @@ public class MessageOperation {
     /**
      * 聊天id
      */
+    @JsonProperty("talk_id")
+    @JsonAlias({"talkId", "talk_id"})
     private String talkId;
 
     /**
      * 消息类型
      */
+    @JsonProperty("message_type")
+    @JsonAlias({"messageType", "message_type"})
     private MessageTypeEnum messageType;
     /**
      * 消息内容
